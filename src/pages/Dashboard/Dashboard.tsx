85ac5c9deb83c8dd53af392fadb8e62f90dd05da
Matheus Wallace da Silva Pereira <matheus.wallace1@outlook.com>
2025-06-01 11:07:43 -0300

    feature: add pizza chart to dashboard

--- src/pages/Dashboard/Dashboard.tsx
+++ src/pages/Dashboard/Dashboard.tsx
@@ -4,6 +4,8 @@ import DayOrdersAmountCard from '@/components/DayOrdersAmountCard'
 import MonthCanceledOrdersAmount from '@/components/MonthCanceledOrdersAmount'
 import MonthRevenueCard from '@/components/MonthRevenueCard'
 import OrderMonthAmountCard from '@/components/OrderMonthAmountCard'
+import PopularProductsChart from '@/components/PoupularProductsChats'
+import RevenueChart from '@/components/RevenueChart'
 
 const Dashboard = () => {
   return (
@@ -16,6 +18,10 @@ const Dashboard = () => {
           <OrderMonthAmountCard />
           <DayOrdersAmountCard />
           <MonthCanceledOrdersAmount />
+        </div>
+        <div className="grid grid-cols-9 gap-4">
+          <RevenueChart />
+          <PopularProductsChart />
         </div>
       </div>
     </>
