edc166686d8e488385da06eb26ad2091d988a9be
Matheus Wallace da Silva Pereira <matheus.wallace1@outlook.com>
2025-06-02 22:01:24 -0300

    feat: add sign-out request and navigating user to login screen

--- src/components/Account-menu.tsx
+++ src/components/Account-menu.tsx
@@ -1,8 +1,10 @@
-import { useQuery } from '@tanstack/react-query'
+import { useMutation, useQuery } from '@tanstack/react-query'
 import { Building, ChevronDown, LogOut } from 'lucide-react'
+import { useNavigate } from 'react-router'
 
 import { getEstablishment } from '@/api/get-managed-establishment'
 import { getProfile } from '@/api/get-profile'
+import { signOut } from '@/api/sign-out'
 
 import StoreProfileDialog from './StoreProfileDialog'
 import { Button } from './ui/button'
@@ -18,6 +20,8 @@ import {
 import { Skeleton } from './ui/skeleton'
 
 const AccountMenu = () => {
+  const navigate = useNavigate()
+
   const { data: profile, isLoading: isLaodingProfile } = useQuery({
     queryKey: ['profile'],
     queryFn: getProfile,
@@ -29,6 +33,13 @@ const AccountMenu = () => {
     queryKey: ['establishment'],
     queryFn: getEstablishment,
     staleTime: Infinity,
+  })
+
+  const { mutateAsync: signOutFn, isPending: isSigningOut } = useMutation({
+    mutationFn: signOut,
+    onSuccess: () => {
+      navigate('/sign-in', { replace: true })
+    },
   })
 
   return (
@@ -70,9 +81,15 @@ const AccountMenu = () => {
               <span className="text-sm">Profile</span>
             </DropdownMenuItem>
           </DialogTrigger>
-          <DropdownMenuItem className="text-rose-500 dark:text-rose-400">
-            <LogOut className="mr-2 h-4 w-4 text-rose-500 dark:text-rose-400" />
-            <span>Exit</span>
+          <DropdownMenuItem
+            asChild
+            className="text-rose-500 dark:text-rose-400"
+            disabled={isSigningOut}
+          >
+            <button className="w-full" onClick={() => signOutFn()}>
+              <LogOut className="mr-2 h-4 w-4 text-rose-500 dark:text-rose-400" />
+              <span>Exit</span>
+            </button>
           </DropdownMenuItem>
         </DropdownMenuContent>
       </DropdownMenu>
