e38e7420619466154619bdaca101c7af80069845
Matheus Wallace da Silva Pereira <matheus.wallace1@outlook.com>
2025-06-21 16:56:49 -0300

    feat: add mocks to profile

--- src/app.tsx
+++ src/app.tsx
@@ -1,3 +1,5 @@
+import './index.css'
+
 import { QueryClientProvider } from '@tanstack/react-query'
 import { Helmet, HelmetProvider } from 'react-helmet-async'
 import { RouterProvider } from 'react-router'
