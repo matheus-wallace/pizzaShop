9c4e6967dec322a07d84e075683dfe1ef92be554
Matheus Wallace da Silva Pereira <matheus.wallace1@outlook.com>
2025-06-21 15:15:08 -0300

    feat: add mocks to sign-in

--- src/main.tsx
+++ src/main.tsx
@@ -1,12 +1,13 @@
-import './index.css'
+import React from 'react'
+import ReactDOM from 'react-dom/client'
 
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-
+import { enableMSW } from './api/mocks'
 import { App } from './app'
 
-createRoot(document.getElementById('root')!).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
-)
+enableMSW().then(() => {
+  ReactDOM.createRoot(document.getElementById('root')!).render(
+    <React.StrictMode>
+      <App />
+    </React.StrictMode>,
+  )
+})
